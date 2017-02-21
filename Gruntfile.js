6c09ac8e52bf066d5cab9d2666f6c57e79a0d567
JYChoi <neocjy@naver.com>
2017-02-21 16:46:01 +0900

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by jychoi on 2017. 2. 9..
+ */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint:{
+            files: ['/js/jquery.securekeyboard.js', 'Gruntfile.js'],
+            options: {
+                globals:{
+                    jQuery: true
+                }
+            }
+        },
+        qunit:{
+            files: ['testDoc.html']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    grunt.registerTask('travis',['qunit']);
+};
